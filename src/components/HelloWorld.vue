2fba0e432cbe3606b377a0357bcd229915e1bba1
Mateja Petrovic <mateja176@gmail.com>
2024-07-19 18:43:12 +0200

    computes value based on valueRef

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ComponentPublicInstance, computed, ref } from 'vue';
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string; }>();
 
-const count = ref(0)
+const inputRef = ref<HTMLInputElement | null>(null);
+const valueRef = ref('');
+const value = computed(() => {
+  // return inputRef.value?.value;
+  return valueRef.value;
+});
+const setInputRef = (e: Element | ComponentPublicInstance | null) => {
+  if (!(e instanceof HTMLInputElement)) {
+    return;
+  }
+  inputRef.value = e;
+};
+const onInput = (e: Event) => {
+  if (!(e.target instanceof HTMLInputElement)) {
+    return;
+  }
+  valueRef.value = e.target.value;
+};
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <input :ref="setInputRef" @input="onInput" />
+  <div>Value: {{ value }}</div>
 </template>
 
 <style scoped>
